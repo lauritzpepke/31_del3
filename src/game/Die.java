4ea34b06a5dbb401b7a78555d39c7d337cddcc8f
s205347 <s205347@student.dtu.dk>
2020-11-15 17:02:01 +0100

    Oprettet en Die class

---
+++ src/game/Die.java
@@ -0,0 +1,26 @@
+package game;
+
+
+import java.util.Random;
+
+public class Die {
+    private int value;
+    private Random r;
+    private int numberOfSides;
+
+    public Die() {
+        r = new Random();
+        numberOfSides = 6;
+    }
+
+    public int roll() {
+        value = r.nextInt(numberOfSides) + 1;
+        return value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+}
