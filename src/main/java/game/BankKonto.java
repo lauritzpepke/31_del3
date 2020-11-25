d4bbe1662ce484e28f1fdb150424bef7d813992e
s205347 <s205347@student.dtu.dk>
2020-11-25 23:47:50 +0100

    Tilføjet javadoc

--- src/main/java/game/BankKonto.java
+++ src/main/java/game/BankKonto.java
@@ -7,14 +7,26 @@ public class BankKonto {
         this.balance = 0;
     }
 
+    /**
+     * Metode for at ændre en spillers balance
+     * @param change Ændring i balancen
+     */
     public void changeBalance(int change) {
         this.balance += change;
     }
 
+    /**
+     * Metode for at set balancen
+     * @param balance En spillers pengebeholdning
+     */
     public void setBalance(int balance) {
         this.balance = balance;
     }
 
+    /**
+     * Metode for at få værdien af balancen
+     * @return den aktuelle balance
+     */
     public int getBalance() {
         return this.balance;
     }
