03df544ee161422fe349edfc0f99c0b3146bb475
s205347 <s205347@student.dtu.dk>
2020-11-25 23:56:16 +0100

    Tilføjet javadoc

--- src/main/java/game/CheckTaber.java
+++ src/main/java/game/CheckTaber.java
@@ -1,7 +1,15 @@
 package game;
 
+/**
+ * Klasse der tjekker om nogen har tabt spillet
+ */
 public class CheckTaber {
 
+    /**
+     *
+     * @param ba En spillers bankkonto
+     * @return om spilleren har tabt eller om spillet skal fortsætte
+     */
     public boolean hasLost(BankKonto ba)
     {
         int balance=ba.getBalance();
