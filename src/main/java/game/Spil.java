9ff187276128060e11ea8fb86709d39965f54ce6
Nadia <s201369@student.dtu.dk>
2020-11-26 17:41:43 +0100

    Jeg gør klar til at gøre gui'en vores. Jeg har tilføjet kommentarer til Spil klassen, som beskriver den logik vi vil opnå.

--- src/main/java/game/Spil.java
+++ src/main/java/game/Spil.java
@@ -1,9 +1,25 @@
 package game;
-import java.util.Scanner;
 public class Spil {
 TerningeKast dieThrow = new TerningeKast();
 
-public static void playGame() {
+public void playGame()
+{
+    // bruger gui
+    // få info fra spillere
+    // MINDST 2 spillere MAKS 4
+
+
+    // opret spillere
+
+    // rækkefølge baseret på alder
+
+
+    // start gui for selve spillet
+
+    // kør spil indtil en går fallit
+
+    //bestem vinder
+
 
 
     }
