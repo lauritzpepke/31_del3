705fece1f4bca35cc4ca1bfae2e62f124ff31d81
s205347 <s205347@student.dtu.dk>
2020-11-26 14:46:32 +0100

    Tilføjet javadoc

--- src/main/java/game/Felt.java
+++ src/main/java/game/Felt.java
@@ -5,6 +5,9 @@ public class Felt {
     private int group, price;
     private int ownerNumber;
 
+    /**
+     * Constructor for felt som skal bruges i bræt-klassen
+     */
     public Felt(String type, int group, String title, int price) {
         this.type = type;
         this.group = group;
@@ -13,27 +16,48 @@ public class Felt {
         this.ownerNumber = 0;
     }
 
+    /**
+     * Metode der angiver ejernummer
+     */
     public void setOwnerNumber() {
         this.ownerNumber = ownerNumber;
     }
 
-
+    /** Metode der henter ejernummer
+     * @return ownerNumber
+     */
     public int getOwnerNumber() {
         return ownerNumber;
     }
 
+    /**
+     * Metode der henter hvilken type feltet er
+     * @return hvilken type feltet er
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Metode som henter gruppenummeret på feltet
+     * @return hvilken gruppe/kategori feltet hører inden under
+     */
     public int getGroup() {
         return group;
     }
 
+    /**
+     * Metode som henter titlen på feltet
+     * @return titlen på feltet
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Metode der henter prisen på feltet
+     * @return prisen på det specifikke felt
+     */
     public int getPrice() {
         return price;
     }
