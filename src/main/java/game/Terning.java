e62a09ebe93c97e933181f70fe6adf36f5f943a9
s205347 <s205347@student.dtu.dk>
2020-11-26 14:50:55 +0100

    Tilføjet javadoc

--- src/main/java/game/Terning.java
+++ src/main/java/game/Terning.java
@@ -3,25 +3,43 @@ package game;
 
 import java.util.Random;
 
+/**
+ * Klassen skaber en 6-sidet terning
+ */
 public class Terning {
     private int value;
     private Random r;
     private int numberOfSides;
 
+    /**
+     * Constructor for terningen som skal bruges i terningekast-klassen
+     */
     public Terning() {
         r = new Random();
         numberOfSides = 6;
     }
 
+    /**
+     * Metode der ruller terningen og returnere værdien
+     * @return værdien som blev fremstillet af at rulle med terningen
+     */
     public int roll() {
         value = r.nextInt(numberOfSides) + 1;
         return value;
     }
 
+    /**
+     * Metode der henter resultatet af roll-metoden
+     * @return value
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * Metode som henter antal sider på terningen
+     * @return antal sider som terningen har
+     */
     public int getNumberOfSides() {
         return numberOfSides;
     }
