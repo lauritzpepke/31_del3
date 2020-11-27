9cdf8a0f63b566deedf85c44596bbd991a19ab9c
s205347 <s205347@student.dtu.dk>
2020-11-27 19:16:06 +0100

    Redigeret spiller-klassens opbygning

--- src/main/java/game/Spiller.java
+++ src/main/java/game/Spiller.java
@@ -8,12 +8,14 @@ public class Spiller {
     private String navn;
     private BankKonto bk;
     private int alder;
-    private int placering = 0;
+    private int nuværendePlacering = 0;
     private static Color farve;
     private boolean fængsel;
     private boolean gratis;
     private boolean frikort;
     private GUI_Car bil;
+    private int rigdom;
+    private int balance;
 
     public Spiller(String navn, int balance, GUI_Car bil) {
         this.navn = navn;
@@ -21,78 +23,101 @@ public class Spiller {
         this.bil = bil;
     }
 
-    public void setNavn(String navn) { this.navn = navn; }
-
     public String getNavn() {
         return navn;
     }
 
-    public void setAlder(int alder) {
-        this.alder = alder;
+    public String toString() {
+        return navn;
     }
 
-    public int getAlder() {
-        return alder;
+    public int getBalance() {
+        return bk.getBalance();
     }
 
-    public void setPlacering(int placering) {
-        this.placering = placering;
+    public void setBalance(int penge) {
+        balance = balance + penge;
     }
 
-    public int getPlacering() {
-        return placering;
+    public int bevægSpiller(int move) {
+        this.nuværendePlacering = nuværendePlacering + move;
+
+        if (nuværendePlacering > 24) {
+            nuværendePlacering = nuværendePlacering - 24;
     }
-
-    public void skiftPlacering() {
-
-    }
-
-    public void setFarve(Color farve) {
-        this.farve = farve;
-    }
-
-    public static Color getFarve() {
-        return farve;
-    }
-
-    public void setFængsel(boolean fængsel) {
-        this.fængsel = fængsel;
-    }
-
-    public boolean isFængsel() {
-        return fængsel;
-    }
-
-    public void setGratis(boolean gratis) {
-        this.gratis = gratis;
-    }
-
-    public boolean isGratis() {
-        return gratis;
-    }
-
-    public void setFrikort(boolean frikort) {
-        this.frikort = frikort;
-    }
-
-    public boolean isFrikort() {
-        return frikort;
-    }
-
-    public boolean getGratis() {
-        return false;
-    }
-
-    public void tilførSkøde(int feltnr, int i) {
-    }
-
-    public boolean ejerEjendom(int makkerFeltnr) {
-        return false;
-    }
-
-    public void ændrLikvideMidler(int i) {
-    }
-
-    public void changeBalance(int i) {
+        return nuværendePlacering;
     }
 }
+
+
+
+
+//    public void setAlder(int alder) {
+//        this.alder = alder;
+//    }
+//
+//    public int getAlder() {
+//        return alder;
+//    }
+//
+//    public void setPlacering(int placering) {
+//        this.placering = placering;
+//    }
+//
+//    public int getPlacering() {
+//        return placering;
+//    }
+//
+//    public void skiftPlacering() {
+//
+//    }
+//
+//    public void setFarve(Color farve) {
+//        this.farve = farve;
+//    }
+//
+//    public static Color getFarve() {
+//        return farve;
+//    }
+//
+//    public void setFængsel(boolean fængsel) {
+//        this.fængsel = fængsel;
+//    }
+//
+//    public boolean isFængsel() {
+//        return fængsel;
+//    }
+//
+//    public void setGratis(boolean gratis) {
+//        this.gratis = gratis;
+//    }
+//
+//    public boolean isGratis() {
+//        return gratis;
+//    }
+//
+//    public void setFrikort(boolean frikort) {
+//        this.frikort = frikort;
+//    }
+//
+//    public boolean isFrikort() {
+//        return frikort;
+//    }
+//
+//    public boolean getGratis() {
+//        return false;
+//    }
+//
+//    public void tilførSkøde(int feltnr, int i) {
+//    }
+//
+//    public boolean ejerEjendom(int makkerFeltnr) {
+//        return false;
+//    }
+//
+//    public void ændrLikvideMidler(int i) {
+//    }
+//
+//    public void changeBalance(int i) {
+//    }
+//}
